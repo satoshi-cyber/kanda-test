2c11727a002d56339c321e9114459bda42efe029
satoshi-cyber <79872416+satoshi-cyber@users.noreply.github.com>
2021-06-16 13:23:41 +0200

    add eslit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'linebreak-style': 'off',
+    "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/explicit-module-boundary-types': "off",
+    'prettier/prettier': [
+    'error',
+    {
+        endOfLine: 'auto',
+    },
+    ],
+  },
+};
